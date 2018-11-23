ef7bef623442b7dbb9d09cda6c0b81a16a48c9b6
github username <github email address>
2018-11-23 14:27:13 -0500

    Done.

--- account.js
+++ account.js
@@ -3,11 +3,11 @@ module.exports = function account() {
   var customerAccount = {
     balance: 0,
     name: 'Azat Mardan',
-    checking: '1'
+    checking: "1"
   }
 
   function setAccountType () {
-    if (customerAccount.checking === 1) {
+    if (customerAccount.checking === "1") {
       customerAccount.type = 'checking'
     } else {
       customerAccount.type = 'savings'
@@ -23,10 +23,12 @@ module.exports = function account() {
     account.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
-  openAccount(customerAccount)
+  openAccount(customerAccount, deposit)
+  debugger
   setAccountType()
   output['Account after opening'] = customerAccount
+  debugger
   return output
 }
